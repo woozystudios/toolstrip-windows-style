359277d6ee7b55b3d69e05bf1bd2ffb0d1a4e44d
woozystudio <woozysstudio23@gmail.com>
2025-12-06 12:29:38 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
